ea1b1a0b91ea403611ffedb8bd96bdae221b2180
Rahat Singh <rahat.singh1617@gmail.com>
2023-07-22 22:33:23 +0530

    initial commit

---
+++ Day 1/dataType.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int a = 8;
+    cout << a <<endl;
+
+    char ch = 's';
+    cout << ch << endl;
+
+    float f = 1.5;
+    cout << f << endl;
+
+    double d = 1.55;
+    cout << f << endl;
+
+    bool  b = true;
+    cout << b << endl;
+
+    bool c = 0;
+    cout << c << endl;
+
+    int size = sizeof(a);
+    cout << "Size of a is:" << size;
+}
